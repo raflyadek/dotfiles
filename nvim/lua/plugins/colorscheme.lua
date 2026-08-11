66490c84ff61df7990db5bbe7fac1be445658841
rafly <raflyade9@gmail.com>
2026-08-11 21:24:55 +0700

    color scheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+-- return {
+--   { "catppuccin/nvim" },
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
+
+return {
+  { "rebelot/kanagawa.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
